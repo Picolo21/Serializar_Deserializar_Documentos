81721d24f98c0cfd0e6d16636aedac026a7a5ab9
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-06-03 19:32:37 -0300

    Add project files.

---
+++ SerializarXML/SerializarXML/Program.cs
@@ -0,0 +1,28 @@
+﻿using Library;
+using System.ComponentModel;
+using System.Xml.Serialization;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string users = "Usuários.xml";
+        string path = @"C:\Users\" + Environment.UserName + @"\";
+
+        string pathFile = path + users;
+
+        User user = new User()
+        {
+            Name = "José Costa Silva",
+            CPF = "123.456.789-12",
+            Email = "josecostasilva@gmail.com"
+        };
+
+        XmlSerializer serializer = new XmlSerializer(typeof(User));
+
+        using (StreamWriter sw = new StreamWriter(pathFile))
+        {
+            serializer.Serialize(sw, user);
+        }
+    }
+}
