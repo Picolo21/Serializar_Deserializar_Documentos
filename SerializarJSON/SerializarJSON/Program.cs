f5120a611a085579f25b1e8bdee9222a3e8fe327
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-06-03 21:29:37 -0300

    serializando objeto para arquivo json

---
+++ SerializarJSON/SerializarJSON/Program.cs
@@ -0,0 +1,27 @@
+﻿using Library;
+using Newtonsoft.Json;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string users = "Usuários.json";
+        string path = @"C:\Users\" + Environment.UserName + @"\";
+
+        string pathFile = path + users;
+
+        User user = new User()
+        {
+            Name = "Maria Costa Silva",
+            Cpf = "123.456.789-12",
+            Email = "mariacostasilva@gmail.com"
+        };
+
+        string serializer = JsonConvert.SerializeObject(user);
+
+        using (StreamWriter sw = new StreamWriter(pathFile))
+        {
+            sw.WriteLine(serializer);
+        }
+    }
+}
